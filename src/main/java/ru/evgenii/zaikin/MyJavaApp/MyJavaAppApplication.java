8ac0b3f511df775719becbec8b84f476cf82f306
Evgenii Zaikin <evgeniizaikin@MacBook-Pro-Evgenij.local>
2024-04-11 17:48:16 +0300

    Initial commit

---
+++ src/main/java/ru/evgenii/zaikin/MyJavaApp/MyJavaAppApplication.java
@@ -0,0 +1,13 @@
+package ru.evgenii.zaikin.MyJavaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/evgenii/zaikin/MyJavaApp/MyJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.evgenii.zaikin.MyJavaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
