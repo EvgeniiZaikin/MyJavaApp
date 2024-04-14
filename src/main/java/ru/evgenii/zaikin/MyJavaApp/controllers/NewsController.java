d000a05df31b3059bd36a403f01a875714445df8
Evgenii Zaikin <evgeniizaikin@MacBook-Pro-Evgenij.local>
2024-04-15 00:15:31 +0300

    Integrate themyleaf into app

--- src/main/java/ru/evgenii/zaikin/MyJavaApp/controllers/NewsController.java
+++ src/main/java/ru/evgenii/zaikin/MyJavaApp/controllers/NewsController.java
@@ -2,36 +2,44 @@ package ru.evgenii.zaikin.MyJavaApp.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import ru.evgenii.zaikin.MyJavaApp.entities.NewsEntity;
 import ru.evgenii.zaikin.MyJavaApp.services.NewsService;
 
+import java.util.Date;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/news")
 @RequiredArgsConstructor
 public class NewsController {
     private final NewsService newsService;
 
-    @PostMapping("/parse")
+    @GetMapping
+    public String home(Model model) {
+        model.addAttribute("serverTime", new Date());
+        return "home";
+    }
+
+    @PostMapping("/api/parse")
     public ResponseEntity<List<NewsEntity>> parse() {
         return ResponseEntity.ok(newsService.parse());
     }
 
-    @GetMapping()
+    @GetMapping("/api/all")
     public ResponseEntity<List<NewsEntity>> getAllNews(@RequestParam(value="status", required=false) String status) {
         if (status == null) return ResponseEntity.ok(newsService.getAllNews());
         else return ResponseEntity.ok(newsService.getNewsByStatus(status));
     }
 
-    @PutMapping("/all/statuses")
+    @PutMapping("/api/all/statuses")
     public ResponseEntity<List<NewsEntity>> updateNewsStatuses(@RequestBody List<NewsEntity> news) {
         return ResponseEntity.ok(newsService.updateNewsStatuses(news));
     }
